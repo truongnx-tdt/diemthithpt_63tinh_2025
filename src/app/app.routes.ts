76214e5c4bef4ab7df35c4cfbb739b2d9a7efe6f
TDT <xuantruong0944@gmail.com>
2025-07-23 11:09:24 +0700

    fix: Correct route configuration in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./app.component').then(m => m.AppComponent),
+    }
+    ,
+    {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: ''
+    }
 ];
